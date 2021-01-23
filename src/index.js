fca4cbf57a6690d1d332371271bbb0b5d9536317
Daniel-KI <dankorshov@gmail.com>
2021-01-23 12:59:15 +0300

    feat: realise check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketsMap = new Map(bracketsConfig);
+  let incorrectBrackets = [];
+
+  for (let i = 0; i < str.length; i++) {
+    str[i] === bracketsMap.get(incorrectBrackets[incorrectBrackets.length-1]) ? incorrectBrackets.pop() : incorrectBrackets.push(str[i]);
+  }
+  return incorrectBrackets.length === 0;
 }
